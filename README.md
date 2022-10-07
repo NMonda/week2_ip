3cb541d1610e9c0d0047620601aefe9eeb272218
NMONDA <ndegemonda@gmail.com>
2022-10-07 13:55:02 +0300

    modified README.md, created  main.css and image files

--- README.md
+++ README.md
@@ -5,16 +5,37 @@ This is a project to create  a web application that takes a user's birthday and 
 Akan names are derived from Ghanian culture. Frequently in Ghana, children are given their first name as a 'day name' which corresponds to the day in the week they were born. Here are Ghanian day names.
 
 ## Author(s) information.
-Reuben Monda 
+**Reuben Monda**
+
 ## Setup instructions 
 1. Clone the project from github
 2. Open the project on a live server
 3. Run the project
-BDD.
-## Technologies used.
+## BDD
+Displays Form For Entering Date and Gender
+INPUT: "User's Birthdate Entered"
+INPUT: "User's Gender Entered"
+OUTPUT: "Displays User's Akan Name According to Submitted Date and Gender"
+Displays an Error Message if Date Input is a future date
+INPUT: "Future Dates"
+OUTPUT: "Unborn person"
+Displays an Error Message if Gender Not Chosen
+INPUT: ""
+OUTPUT: "You must select your gender"
+Resets Form Input Upon Successful Data Validation
+INPUT: "Clicks Submit Button"
+OUTPUT: "Resets Form and Displays Akan Name and Day of Week"
 
-## Contact information.
+## Technologies used.
+HTML - which was used to build the structure of the web pages.
+
+CSS - which was used to style the web pages.
+
+JavaScript - Which was used to make the web pages interactive with the client-side and server-side, and provide accurate feedback as required by the clients.
+BOOTSTRAP 
+
+## Live links
+https://nmonda.github.io/week2_ip
 
 ## License and Copyright information.
 © 2022 Reuben, Nmonda 
-**Reuben Monda**
---
+++ style.css/script.js
@@ -0,0 +1,93 @@
+let malenames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+let femalenames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+let dayofweek =["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+
+function calc() {
+
+var DD = document.getElementById("Day").value;
+var year = document.getElementById("Year").value;
+var MM = document.getElementById("Month").value;
+var male = document.getElementById("Man").checked;
+var female = document.getElementById("Woman").checked;
+
+
+var YY = year.slice(0, 2);
+var YY = year.slice(2, 4);
+var CC = (YY - 1) / 100 + 1;
+var output= parseInt(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+
+if (MM < 1 || MM > 12 || MM == 2 && DD > 29) {
+  alert("PLEASE INSERT A VALID MONTH");
+
+} else if (DD < 1 || DD > 31) {
+  alert("PLEASE INSERT A VALID DAY");
+}
+
+
+  
+if (female) {
+  if (output == 1) {
+    document.getElementById("output").innerHTML =
+    alert("Hey Missy!! You were born on a Monday, you're  Akan name is " + femalenames[1]);
+} else if (output == 2) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Missy!! You were born on a Tuesday, you're  Akan name is " + femalenames[2]);
+} else if (output == 3) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Missy!! You were born on a Wednesday, you're  Akan name is " + femalenames[3]);
+} else if (output == 4) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Missy!! You were born on a Thursday, you're  Akan name is " + femalenames[4]);
+} else if (output == 5) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Missy!! You were born on a Friday, you're  Akan name is " + femalenames[5]);
+} else if (output == 6) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Missy!! You were born on a Saturday, you're  Akan name is " + femalenames[6]);
+} else if (output == 0) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Missy!! You were born on a Sunday, you're  Akan name is " + femalenames[0]);
+}
+}
+
+ 
+
+else if (male) {
+  if (output == 1) {
+    document.getElementById("output").innerHTML =
+    alert("Hey Buddy!! You were born on a Monday, you're  Akan name is " + malenames[1]);
+} else if (output == 2) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Buddy!! You were born on a Tuesday, you're  Akan name is " + malenames[2]);
+} else if (output == 3) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Buddy!! You were born on a Wednesday, you're  Akan name is " + malenames[3]);
+} else if (output == 4) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Buddy!! You were born on a Thursday, you're  Akan name is " + malenames[4]);
+} else if (output == 5) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Buddy!! You were born on a Friday, you're  Akan name is " + malenames[5]);
+} else if (output == 6) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Buddy!! You were born on a Saturday, you're  Akan name is " + malenames[6]);
+} else if (output == 0) {
+  document.getElementById("output").innerHTML =
+    alert("Hey Buddy!! You were born on a Sunday, you're  Akan name is " + malenames[0]);
+}
+} else {
+alert("INVALID INPUT,PLEASE INSERT YOUR DETAILS BELOW!");
+}
+}  
+
+
+
+(document).ready(function () {
+
+('#Form').submit(function (a) {
+  
+  a.refresh();
+
+});
+});
